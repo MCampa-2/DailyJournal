6dcf022c6761ec2bec8f3dbef5e78fe2b99147dc
Michael Campagnoli <michael.campagnoli@protonmail.com>
2023-08-04 21:55:23 -0400

    Add .gitidnore

--- app.js
+++ app.js
@@ -68,10 +68,17 @@ app.get("/posts/:entry", async (req, res) => {
   }
   
   });
+
+
+  let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+
   
 
-app.listen("3000", function(req,res){
-    console.log("Server listening on port 3000");
+app.listen(port, function(req,res){
+    console.log("Server listening successfully");
 });
 
 
